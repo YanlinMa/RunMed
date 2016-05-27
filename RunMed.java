240ea3e5f6702c6fc71ec2653340f5d5b3733248
Dorothy Ng <dng1@stuy.edu>
2016-05-26 22:15:42 -0400

    it's only returning zeroes

--- RunMed.java
+++ RunMed.java
@@ -51,7 +51,7 @@ public class RunMed {
      *****************************************************/
     public void insert( int addVal )
     {
-    	if (isEmpty) leftHeap.add(addVal);
+    	if (isEmpty()) leftHeap.add(addVal);
     	else if (addVal <= leftHeap.peekMax()) leftHeap.add(addVal);
     	else rightHeap.add(addVal);
     }//O(?)
